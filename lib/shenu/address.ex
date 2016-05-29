c1681e1ee0c2a5d3fe791dc6833641d1343c1195
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-05-28 18:42:47 -0600

    add address impl

--- lib/shenu/address.ex
+++ lib/shenu/address.ex
@@ -6,4 +6,20 @@ defmodule Shenu.Address do
             district: nil,
             region: nil,
             postcode: nil
+
+  def new(opts \\ []) do
+    struct(__MODULE__, opts)
+  end
+end
+
+defimpl Shenu, for: Shenu.Address do
+  ## TODO support distance with google maps, maybe?
+  ##      for now, we'll just use a textual difference
+  def difference(a, b, method) do
+    Enum.reduce(unquote(Map.from_struct(%Shenu.Address{}) |> Map.keys()), 0, fn(key, acc) ->
+      a_v = a |> Map.get(key) |> to_string()
+      b_v = b |> Map.get(key) |> to_string()
+      acc + Shenu.BitString.difference(a_v, b_v, method)
+    end)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule Hieroglyph.Mixfile do
+defmodule Shenu.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :hieroglyph,
+    [app: :shenu,
      version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
